6d22fab73857da8e10f7570e9625c4e37b2d1660
Karolina Seidlova <35819148+BcKarolinaSeidlova@users.noreply.github.com>
2024-04-16 23:13:03 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,12 +2,17 @@
 
 var menu = document.getElementById("Menu");
 var odkazy = document.getElementById("Odkazy");
-
+var button = document.getElementById("Button");
 menu.addEventListener("click", Show);
 
 function Show () {
  if (odkazy.style.getPropertyValue("display") == "none") {
-  odkazy.style.setProperty("display", "block")} 
- else {
-   odkazy.style.setProperty("display", "none")}
+  odkazy.style.setProperty("display", "flex");
+  button.style.setProperty("color","black");
+  button.style.setProperty("background","cyan");
+} else {
+  odkazy.style.setProperty("display", "none");
+  button.style.setProperty("color","#f4ddba");
+  button.style.setProperty("background","black");
+      }
 }
